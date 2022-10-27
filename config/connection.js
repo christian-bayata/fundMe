3a54efd1fcda3f8331b91e3d4515f0c4293d4be7
christian-bayata <franksagie1@gmail.com>
2022-10-27 15:25:23 +0100

    Setup mongoose databse connection

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const environment = process.env.NODE_ENV || "development";
+let connectionString;
+
+switch (environment) {
+  case "production":
+    // Point the database credentials to the production DB here
+    connectionString = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+    break;
+  case "testing":
+    connectionString = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.TEST_DB_NAME}`;
+    break;
+  default:
+    connectionString = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+}
+
+module.exports = connectionString;
