91bbe5b3c19ba140f7c44b527697c5c1cdd93c2c
christian-bayata <franksagie1@gmail.com>
2022-10-27 16:01:40 +0100

    Error handler middleware function for development environment yet

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require("express-async-errors");
 require("dotenv").config();
 const express = require("express");
 const swaggerUi = require("swagger-ui-express");
 const morgan = require("morgan");
 const winston = require("winston");
+const error = require("./middlewares/error");
 
 /* Initialize express application */
 const app = express();
@@ -10,5 +12,8 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(morgan("combined", { stream: winston.stream }));
 app.use(express.json());
 app.use(cors());
+
+/* Use the error handling middleware as the last in the middleware stack */
+app.use(error);
 
 module.exports = app;
