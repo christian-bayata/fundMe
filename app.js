d70939e37e043173a588009978a248b3dac1c326
christian-bayata <franksagie1@gmail.com>
2022-10-27 15:30:02 +0100

    Initialized server file

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const swaggerUi = require("swagger-ui-express");
 const morgan = require("morgan");
 const winston = require("winston");
 
-const port = process.env.PORT || 5000;
-
 /* Initialize express application */
 const app = express();
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(morgan("combined", { stream: winston.stream }));
 app.use(express.json());
 app.use(cors());
+
+module.exports = app;
