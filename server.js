9dd9eeee19d79e2e9f25508a6228ba2e01b4249b
christian-bayata <franksagie1@gmail.com>
2022-10-27 16:01:20 +0100

    Error handler middleware function for development environment yet

--- server.js
+++ server.js
@@ -2,4 +2,19 @@ const app = require("./app");
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+//Error handler for uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err.stack}`);
+  console.log("Shutting down due to uncaught exception");
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port} in environment ${mode}`));
+
+//Error handler for unhandled rejections
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err.stack}`);
+  console.log("Shutting down due to unhandled rejections");
+  server.close(() => {
+    process.exit(1);
+  });
+});
