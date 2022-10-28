c9d6ee6a8d9b3c4ab9479c62747e92fa39e7bc75
christian-bayata <franksagie1@gmail.com>
2022-10-28 10:42:33 +0100

    Added User model with bcrypt and jwt encyrption

---
+++ models/user.js
@@ -0,0 +1,59 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      es_indexed: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      es_indexed: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      es_indexed: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+      unique: true,
+      es_indexed: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+/* Generate JSON web token for user */
+UserSchema.methods.generateJsonWebToken = function () {
+  return jwt.sign(
+    {
+      userId: this._id,
+      email: this.email,
+      isAdmin: this.isAdmin,
+    },
+    process.env.JWT_SECRET_KEY
+  );
+};
+
+/* Compare password using bcrypt.compare */
+userSchema.methods.comparePassword = async function (userPassword) {
+  return await bcrypt.compare(userPassword, this.password);
+};
--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const app = require("./app");
-
-const port = process.env.PORT || 5000;
-const environment = process.env.NODE_ENV || "development";
+const Database = require("./config/database");
 
 //Error handler for uncaught exceptions
 process.on("uncaughtException", (err) => {
@@ -9,6 +7,13 @@ process.on("uncaughtException", (err) => {
   console.log("Shutting down due to uncaught exception");
 });
 
+/* Connect to the database */
+const connectionString = require("./config/connection");
+new Database(connectionString).connect();
+
+/* Declare port and environment, then run the server */
+const port = process.env.PORT || 5000;
+const environment = process.env.NODE_ENV || "development";
 let server = app.listen(port, () => console.log(`Server is running on port ${port} in ${environment} mode`));
 
 //Error handler for unhandled rejections
