1626846177fe4ae3040529af43f47d0d26d7d2cd
christian-bayata <franksagie1@gmail.com>
2022-11-17 15:44:36 +0100

    Completed API to fund other people's account using a flag

--- api/v1/controllers/transaction.js
+++ api/v1/controllers/transaction.js
@@ -23,21 +23,43 @@ const fundAccount = async (req, res) => {
     session.startTransaction();
 
     if (flag == "my_account") {
-      // if (!accountNum) return Response.sendError({ res, statusCode: status.BAD_REQUEST, message: "Provide the account number" });
-
-      const getAccount = await accountRepository.findAccount({ user: user._id }); /* : await accountRepository.findAccount({ accountNum }); */
+      const getAccount = await accountRepository.findAccount({ user: user._id });
       if (!getAccount) return Response.sendError({ res, statusCode: status.NOT_FOUND, message: "Account does not exist" });
 
+      /* Calculate the charge */
       const charge = helper.calcCharges({ accountId: getAccount._id, userId: user._id, flag: "my_account", amount });
 
       /* Create the transaction */
       const createdTransaction = await transRepository.fundAccount([charge], { session });
 
       /* Update the account schema */
-
       await accountRepository.updateAccount({ $inc: { available: charge.totalAmount, total: charge.totalAmount }, dateOfLastAction: Date.now() }, getAccount._id, { session });
 
       return Response.sendSuccess({ res, statusCode: status.OK, message: "Successfully funded your account", body: createdTransaction });
+    }
+
+    if (flag == "other_account") {
+      if (!accountNum) return Response.sendError({ res, statusCode: status.BAD_REQUEST, message: "Provide the account number" });
+
+      const getMyAccount = await accountRepository.findAccount({ user: user._id });
+      if (!getMyAccount) return Response.sendError({ res, statusCode: status.NOT_FOUND, message: "Your account does not exist" });
+
+      const getOtherAccount = await accountRepository.findAccount({ accountNum });
+      if (!getOtherAccount) return Response.sendError({ res, statusCode: status.NOT_FOUND, message: "Other account does not exist" });
+
+      /* Calculate the charge */
+      const charge = helper.calcCharges({ accountId: getMyAccount._id, userId: user._id, flag: "other_account", amount });
+
+      /* Create the transaction */
+      const createdTransaction = await transRepository.fundAccount([charge], { session });
+
+      /* Update the account schema */
+      /* Increase the receiver's money in the account */
+      await accountRepository.updateAccount({ $inc: { available: charge.totalAmount, total: charge.totalAmount }, dateOfLastAction: Date.now() }, getOtherAccount._id, { session });
+      /* Decrease the sender's money in the account */
+      await accountRepository.updateAccount({ $inc: { available: -charge.totalAmount, total: -charge.totalAmount }, dateOfLastAction: Date.now() }, getMyAccount._id, { session });
+
+      return Response.sendSuccess({ res, statusCode: status.OK, message: "Successfully funded other account", body: createdTransaction });
     }
   } catch (error) {
     console.log(error);
